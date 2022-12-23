c4752d58bd8a78fbf40e856a9ddec43fb6c8ba82
ofelmy <57227589+ofelmy@users.noreply.github.com>
2022-12-23 20:41:01 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import random
+
+class settler():
+    next_id = 0
+    
+    def __init__(self, age=None, gender=None):
+        self.id = settler.next_id
+        settler.next_id += 1
+
+        if age is None:
+            self.age = self.random_age()
+        else: 
+            self.age = age
+        
+        if gender is None:
+            self.gender = self.random_gender()
+        else:
+            self.gender = gender
+        
+    
+    def show(self):
+        print(f"id: {self.id}, age: {self.age}, genre: {self.gender}")
+
+    def random_age(self): 
+        return random.randint(0, 100)
+
+    def random_gender(self):
+        return random.choice(["man", "woman"])
+    
+    def make_old(self):
+        self.age += 1
+        return  self.age
+
+    
+initial_settlers_number = 5
+settlers_list = []
+
+settlers_list = [settler() for i in range(initial_settlers_number)]
+
+for i in settlers_list:
+    i.show()
+    i.make_old()
+    i.show()
