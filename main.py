44a6324d03634d47ac0b48f6e9438636b937bb3e
olivier <ofelmy0204@gmail.com>
2022-12-25 18:14:40 +0100

    amélioration du code pour l'affichage. La scrollbar n'apparait toujours pas tout le temps

--- main.py
+++ main.py
@@ -4,31 +4,39 @@ from prettytable import PrettyTable
 import msvcrt
 import tkinter as tk
 import matplotlib.pyplot as plt
-from functions.measure import *
-from functions.proba import *
 from functions.show import *
 from functions.settlers import *
 from functions.evolution import *
 from vars.main import *
-
-
-
 
 settlers_list = [settler() for i in range(initial_settlers_number)]
 jour = 0
 age_max = 0
 os.system("cls")
 
+
+
 table = PrettyTable()
 table.field_names = ["id", "age", "gender", "state"]
 
+# create root window
 root = tk.Tk()
-root.geometry("480x640")
-text = tk.Text(root, yscrollcommand=True)
-text.pack()
-scrollbar = tk.Scrollbar(root, command=text.yview)
+root.geometry("640x640")
+
+frame = tk.Frame(root)
+
+# create text zone named "data_show"
+data_show = tk.Text(frame)
+scrollbar = tk.Scrollbar(frame, command=data_show.yview)
+
+# add text zone in the root windows
+data_show.pack(side="left", fill="both", expand=False)
 scrollbar.pack(side="right", fill="y")
-text.config(yscrollcommand=scrollbar.set)
+
+data_show.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=data_show.yview)
+
+frame.pack(fill="both", expand=True)
 
 # plt.hist(jour, initial_settlers_number,alpha=0.5,color='black')
 # plt.show(block=False)
@@ -45,8 +53,8 @@ while len(settlers_list) > 0:
     string_to_print += "\n" + str(get_table(table, settlers_list, 20))
     # plt.hist(jour, len(settlers_list),alpha=0.5,color='black')
     # plt.draw()
-    text.delete("1.0", "end")
-    text.insert("end", string_to_print)
+    data_show.delete("1.0", "end")
+    data_show.insert("end", string_to_print)
     
     root.update()
 
